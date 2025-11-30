1bb7c9830db91ff1fc915843fcb065eae9fd9be3
JuliusBourbon <naherrrrr@gmail.com>
2025-11-30 09:18:49 +0700

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import DataGempa from './components/dataGempa'
+
+export default function App(){
+  return(
+    <DataGempa/>
+  )
+}
---
+++ src/components/dataGempa.jsx
@@ -0,0 +1,7 @@
+export default function DataGempa(){
+    return(
+        <div>
+            <h1>Tes</h1>
+        </div>
+    )
+}
