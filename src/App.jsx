937a46e2753891e62e6968c46026d721ddcf6999
JuliusBourbon <naherrrrr@gmail.com>
2025-11-30 14:17:16 +0700

    React Router Config

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Routes, Route, useLocation  } from 'react-router-dom'
 import './App.css'
 import DataGempa from './components/dataGempa'
+import Leaflet from './components/leaflet'
 
 export default function App(){
   return(
-    <DataGempa/>
+    <div>
+
+      <main>
+        <Routes>
+          <Route path='/' element={<Leaflet/>}/>
+          <Route path='/data' element={<DataGempa/>}/>
+        </Routes>
+
+      </main>
+    </div>
   )
 }
