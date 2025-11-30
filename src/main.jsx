947031346e4b66a10bb8cef9e88c80efbd3fcc0d
JuliusBourbon <naherrrrr@gmail.com>
2025-11-30 11:21:34 +0700

    Leaflet Config

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41]
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
