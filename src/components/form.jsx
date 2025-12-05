71e5a61c8f60a17fd1c25a10a68882cc12bc2edd
JuliusBourbon <naherrrrr@gmail.com>
2025-12-06 04:49:14 +0700

    update report form

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,4 +1,43 @@
+import { useState } from "react"
+
 export default function Form({onClose}) {
+    const [formData, setFormData] = useState({
+        title: '',
+        user_name: '',
+        description: '',
+        type: 'Banjir'
+    });
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const dataToSend = {
+            ...formData,
+            user_id: 1,
+            latitude: -6.2088,
+            longitude: 106.8456,
+            location_name: "Jakarta (Dummy)" 
+        };
+
+        try {
+            const response = await fetch('http://localhost:3000/api/reports', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(dataToSend)
+            });
+
+            if (response.ok) {
+                alert("Report Sent!");
+            } else {
+                alert("Failed to sending report!");
+            }
+
+        } catch (error) {
+            console.error("Connection Error:", error);
+        }
+    };
     return(
         <div className="bg-white rounded-lg w-[90%] h-[80%] relative pointer-events-auto border shadow-2xl">
             <div className="absolute z-1 w-full">
@@ -9,6 +48,25 @@ export default function Form({onClose}) {
             <div>
                 <h1>Form</h1>
             </div>
+            <form onSubmit={handleSubmit} className="p-4 bg-white shadow rounded">
+                <input 
+                    type="text" 
+                    placeholder="Judul Laporan"
+                    className="border p-2 w-full mb-2"
+                    onChange={(e) => setFormData({...formData, title: e.target.value})}
+                />
+                
+                <input 
+                    type="text" 
+                    placeholder="Nama Samaran (Opsional)"
+                    className="border p-2 w-full mb-2"
+                    onChange={(e) => setFormData({...formData, user_name: e.target.value})}
+                />
+
+                <button type="submit" className="cursor-pointer bg-red-600 text-white p-2 rounded">
+                    Kirim Laporan 🚀
+                </button>
+            </form>
         </div>
     )
 }
