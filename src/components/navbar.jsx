cdb4c5cc55ec6eccd26a133dea842875ead142cb
JuliusBourbon <naherrrrr@gmail.com>
2025-12-17 21:07:44 +0700

    Update navbar UI

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,15 +11,15 @@ export default function Navbar({ activeTab, setActiveTab, onOpenData, onOpenForm
     }, [location.pathname, setActiveTab]);
 
     const navClass = (tabName) => {
-        const baseClass = "cursor-pointer px-5 py-3 rounded-3xl transition-all duration-300";
-        const activeClass = "bg-blue-950 font-bold shadow-inner";
-        const inactiveClass = "hover:bg-blue-600";
+        const baseClass = "cursor-pointer px-5 py-3 rounded-[36px] transition-all duration-300";
+        const activeClass = "bg-[#D9D9D9]/25 font-bold shadow-[inset_0px_3px_4px_rgba(0,0,0,0.35)]";
+        const inactiveClass = "hover:bg-[#D9D9D9]/15";
 
         return `${baseClass} ${activeTab === tabName ? activeClass : inactiveClass}`;
     };
     return(
         <div className="flex pointer-events-none justify-center mt-7 relative z-50">
-            <div className="bg-blue-800 text-white rounded-3xl pointer-events-auto shadow-xl">
+            <div className="bg-linear-to-b from-[#0028ac] to-[#008CFF] text-white rounded-[36px] pointer-events-auto shadow-xl p-2">
                 <div className="flex gap-1">
                     <NavLink className={navClass('home')} onClick={() => setActiveTab('home')} to='/'>
                         <h1>Home</h1>
